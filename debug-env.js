b46effe853d9bb888eca8fffa9f86d8c4920a247
DanielGhanabari <danielghanbari7@gmail.com>
2025-09-19 13:39:15 +0100

    Revert to HTTP endpoint: VPS SSL not configured properly

--- debug-env.js
+++
@@ -1,5 +0,0 @@
-// Debug environment variables during build
-console.log('Environment variables during build:');
-console.log('VITE_API_BASE_URL:', process.env.VITE_API_BASE_URL);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('All VITE_ vars:', Object.keys(process.env).filter(key => key.startsWith('VITE_')).map(key => `${key}: ${process.env[key]}`));
